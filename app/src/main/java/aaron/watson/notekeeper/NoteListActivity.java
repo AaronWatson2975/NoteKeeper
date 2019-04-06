d65244e73b7adf17e4ad2666bd41760b69c83c2f
Aaron <AaronWatson2975@gmail.com>
2019-04-06 10:05:24 -0230

    Linked ListView with the notes contained in the DataManager

--- app/src/main/java/aaron/watson/notekeeper/NoteListActivity.java
+++ app/src/main/java/aaron/watson/notekeeper/NoteListActivity.java
@@ -1,12 +1,16 @@
 package aaron.watson.notekeeper;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
@@ -32,7 +36,9 @@ public class NoteListActivity extends AppCompatActivity {
     private void initializeDisplayContent() {
         ListView listNotes = findViewById(R.id.list_notes);
 
-        
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        listNotes.setAdapter(adapterNotes);
     }
 
 }
