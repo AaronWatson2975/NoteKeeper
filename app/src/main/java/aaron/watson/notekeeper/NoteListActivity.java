da96f650d580255e5cd424c462d9bd63c43fc822
Aaron Watson <AaronWatson2975@gmail.com>
2019-04-09 19:54:35 -0230

    Replaced ListView with RecyclerView in NoteList Activity

--- app/src/main/java/aaron/watson/notekeeper/NoteListActivity.java
+++ app/src/main/java/aaron/watson/notekeeper/NoteListActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,7 +16,7 @@ import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
-    private ArrayAdapter<NoteInfo> mAdapterNotes;
+    // private ArrayAdapter<NoteInfo> mAdapterNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +39,11 @@ public class NoteListActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mAdapterNotes.notifyDataSetChanged();
+        // mAdapterNotes.notifyDataSetChanged();
     }
 
     private void initializeDisplayContent() {
-        final ListView listNotes = findViewById(R.id.list_notes);
+       /* final ListView listNotes = findViewById(R.id.list_notes);
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
@@ -56,7 +58,11 @@ public class NoteListActivity extends AppCompatActivity {
                 intent.putExtra(NoteActivity.NOTE_POSITION, position);
                 startActivity(intent);
             }
-        });
+        }); */
+
+       final RecyclerView recyclerNotes = findViewById(R.id.list_notes);
+       final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
+       recyclerNotes.setLayoutManager(notesLayoutManager);
     }
 
 }
