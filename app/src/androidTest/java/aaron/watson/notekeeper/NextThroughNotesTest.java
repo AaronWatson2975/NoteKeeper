089d882b6889b44f1dd397aae4c65cce8aec54f2
Aaron <AaronWatson2975@gmail.com>
2019-04-13 21:37:03 -0230

    Added Navigation Drawer Tests

--- app/src/androidTest/java/aaron/watson/notekeeper/NextThroughNotesTest.java
+++ app/src/androidTest/java/aaron/watson/notekeeper/NextThroughNotesTest.java
@@ -1,9 +1,16 @@
-package aaron.watson.notekeeper;
+/* package aaron.watson.notekeeper;
 
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.RecyclerView;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.*;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.*;
@@ -22,6 +29,27 @@ public class NextThroughNotesTest {
 
     @Test
     public void NextThroughNotes() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_notes)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+
+        for(int index = 0; index < notes.size(); index++) {
+            NoteInfo note = notes.get(index);
+
+            onView(withId(R.id.spinner_courses)).check(
+                    matches(withSpinnerText(note.getCourse().getTitle()));
+
+            onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+            onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+
+            if(index < notes.size() - 1) {
+                onView(allOf(withId(R.id.action_next), isEnabled())).perform(click());
+            }
+        }
+        onView(withId(R.id.action_next)).check(matches(not(isEnabled())));
+        pressBack();
     }
 }
+*/
